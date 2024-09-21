edcd216ba58e72778b3defe098dfabd5e5644792
Artur Kopytin <51398263+Asnvir@users.noreply.github.com>
2024-09-21 16:19:27 +0300

    Time: 60 ms (94.14%), Space: 58.9 MB (76.58%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,14 +13,15 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
+    if (!root) {
+            return null;
+        }
     if (root.val < p.val && root.val < q.val) {
-        return lowestCommonAncestor(root.right,p,q);
+        return lowestCommonAncestor(root.right, p, q);
     }
-
     if (root.val > p.val && root.val > q.val) {
-        return lowestCommonAncestor(root.left,p,q);
+        return lowestCommonAncestor(root.left, p, q);
     }
-
     return root;
 };
 
