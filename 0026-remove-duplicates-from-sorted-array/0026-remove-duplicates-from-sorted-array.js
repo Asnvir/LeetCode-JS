b54e3817f75516528553099ceeee3083fb8cda57
Artur Kopytin <asnvir@gmail.com>
2025-04-02 22:52:25 +0300

    Time: 0 ms (100%), Space: 57.6 MB (43.91%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+        if (nums.length === 0) {
+        return 0;
+    }
+
+    let slow = 0;
+
+    for (let fast = 1; fast < nums.length; fast++) {
+        if (nums[slow] !== nums[fast]) {
+            nums[++slow] = nums[fast];
+        }
+    }
+
+    return slow + 1;
+};
