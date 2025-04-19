ab44099641c6ff1cc7d09b77d7cc31b147e9b624
Artur Kopytin <asnvir@gmail.com>
2025-04-19 17:27:24 +0300

    Time: 2 ms (73.89%), Space: 63.4 MB (76.39%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-   let maxSum = nums[0];
-   let currentSum = nums[0];
+    let currSum = nums[0];
+    let maxSum = nums[0];
 
-   for(let i=1; i<nums.length; i++){
-    currentSum = Math.max(nums[i], currentSum+nums[i]);
-    maxSum = Math.max(currentSum, maxSum);
-   }
+    for (let i = 1; i < nums.length; i++) {
+        currSum = Math.max(nums[i], currSum + nums[i]);
+        maxSum = Math.max(maxSum, currSum);        
+    }
 
-   return maxSum;
+    return maxSum;
 };
